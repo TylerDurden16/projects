e674afbdccae655950cd9a332b209624d698e3c0
TylerDurden16 <larrysellers16@gmail.com>
2022-03-20 14:54:31 -0700

    updated rock-paper-scissors.js and html UI ver., with no bugs

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -15,7 +15,7 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
 playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You Lose! Paper beats Rock';
+        return 'You lose! Paper beats Rock';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'You win! Rock beats Scissors';
     } else if (playerSelection === 'rock' && computerSelection === 'rock') {
