077d77cae8cff4e9aff8466b8d21b107d071f24c
TylerDurden16 <larrysellers16@gmail.com>
2022-04-29 16:38:13 -0700

    added calculator-ver.2 js,html. index positions of operators logged

---
+++ calculator/calculator-ver.2.js
@@ -0,0 +1,185 @@
+let total = "";
+const numArray = [];
+const operArray = [];
+let operUsed = false;
+let operator;
+let number = 0;
+let numAdded = false;
+let removedArrEl;
+let sum = 0;
+const operPos = {
+    multiply: [],
+    divide: [],
+    add: [],
+    subtract: []
+}
+const multipyPos = [];
+const dividePos = [];
+const addPos = [];
+const subPos = [];
+let numInput = document.getElementById("numInput");
+let currTotal = document.getElementById("currTotal");
+currTotal.style.display = "none";
+
+const erase = e => {
+    numInput.textContent = "0";
+    total = "";
+    currTotal.textContent = "";
+    numArray.splice(0, numArray.length);
+    operArray.splice(0, operArray.length);
+}
+
+const totalNum = () => {
+        //let sum = 0;
+      //  for (let i = 0; ; i++){
+       /* if (operArray[0] ==="/") {
+            sum = numArray[0] / numArray[1];
+        } else if (operArray[0] === "*") {
+            sum = numArray[0] * numArray[1];
+        } else if (operArray[0] === "-") {
+            sum = numArray[0] - numArray[1];
+        } else if (operArray[0] === "+") {
+            sum = numArray[0] + numArray[1];
+        } else {
+            console.log("Error in totalNum()");
+        }*/
+
+    //}
+operArray.map((oper, index) => {
+    if (oper === "m") {
+      multipyPos.push(index);
+      operPos.multiply.push(index);
+    } if (oper === "d") {
+        dividePos.push(index);
+        operPos.divide.push(index);
+    } if (oper === "a") {
+        addPos.push(index);
+        operPos.add.push(index);
+    } if (oper === "s") {
+        subPos.push(index);
+        operPos.subtract.push(index);
+    }
+});
+
+    console.log(operPos);
+    if (operArray[1] === "=") {
+        //numArray.splice(0, 2);
+        //operArray.splice(0, 2);
+        console.log(numArray);
+        console.log(operArray);
+        //currTotal.textContent = sum;
+        numInput.textContent = sum;
+        console.log(sum);
+        return sum;
+    }
+    //numArray[0] = sum;
+    //numArray.pop();
+    
+    //operArray[0] = operArray[1];
+    //operArray.pop();
+    operUsed = false;
+    numInput.textContent =sum;
+    //console.log(numArray);
+    //return sum;
+}
+
+const addValues = e => {  
+     total += e.target.value || e.key;
+     numInput.textContent = total;
+
+     numAdded = true;   
+}
+
+const checkOperators = () => {
+    if (numAdded === false) {
+        numArray.splice(1, 1);
+        console.log(numArray);  
+    }
+    if (operUsed === true && operArray[1]) {
+        //operArray[0] = operArray[1];
+        //operArray.pop();
+        console.log(operArray);
+    }
+
+}
+
+const backSpace = (e) => {
+    if (!total) {
+        total = "";
+        numInput.textContent = "0";
+        return;
+    } else {
+        total = total.slice(0, total.length -1);
+        numInput.textContent = total;
+    }
+      console.log(total);
+}
+
+const addOperators = e => {
+   
+        checkOperators();
+        if (numAdded === true) {   
+        numArray.push(parseInt(total));
+        total = "";  
+        }
+        if (isNaN(numArray[0] && numAdded === true)) {
+           numArray.splice(0,1);
+        } 
+           
+        console.log(numArray);
+        
+        numAdded = false;  
+     if (e.target.id === "divide" || e.key === "/") {
+        operArray.push("d");
+        operator = "d";
+        operUsed = true;
+    } else if (e.target.id === "multiply" || e.key  === "*") {
+        operArray.push("m");
+        operator = "m";
+        operUsed = true;
+    } else if (e.target.id === "subtract" || e.key === "-") {
+        operArray.push("s");
+        operator = "s";
+        operUsed = true;
+    } else if (e.target.id === "add" || e.key === "+") {
+        operArray.push("a");
+        operator = "a";
+        operUsed = true;
+    } else if (e.target.id === "equals" || e.key === "=" || e.key === "Enter") {
+        operArray.push("=");
+    } else {
+        console.log("Error in addOperators()");
+    }
+    if (operArray.length === 6) {
+        totalNum();
+    }
+   // totalNum();
+    console.log(operArray);
+}
+
+const keyInput = e => {
+    const key = e.key;
+    //console.log(e.key);
+    if (key === "0" || key === "1" || key === "2" || key === "3" || key === "4" || key === "4"
+   || key === "5" || key === "6" || key === "7" || key === "8" || key === "9" || key === "."){
+       addValues(e);
+   } if (key === "+" || key === "-" || key === "*" || key === "/" || key === "=" || key === "Enter") {
+       addOperators(e);
+   } if (key === "Backspace") {
+       backSpace(e);
+   }
+    else {
+       return;
+   }
+}
+
+const numBtn = document.querySelectorAll(".numBtn");
+const clrBtn = document.getElementById("clrBtn");
+const operBtn = document.querySelectorAll(".operBtn");
+const bckSpace = document.getElementById("backSpace");
+
+numBtn.forEach(numbers => numbers.addEventListener("click", addValues));
+operBtn.forEach(operators => operators.addEventListener("click", addOperators))
+clrBtn.addEventListener("click", erase);
+window.addEventListener("keydown", keyInput);
+bckSpace.addEventListener("click", backSpace);
