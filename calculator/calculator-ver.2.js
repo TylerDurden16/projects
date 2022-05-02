c368fdf1d5fdfd3054fd1efeee1348bc8189ac11
TylerDurden16 <larrysellers16@gmail.com>
2022-05-01 18:41:49 -0700

    updated calculator-ver.2.js. Fixed multiplication/division. Errors with addition/subtraction

--- calculator/calculator-ver.2.js
+++ calculator/calculator-ver.2.js
@@ -20,6 +20,7 @@ const subPos = [];
 let numInput = document.getElementById("numInput");
 let currTotal = document.getElementById("currTotal");
 currTotal.style.display = "none";
+const arrStr = operArray.join("");
 
 const erase = e => {
     numInput.textContent = "0";
@@ -28,28 +29,13 @@ const erase = e => {
     numArray.splice(0, numArray.length);
     operArray.splice(0, operArray.length);
 }
-/*const checkOper = operArray.map((oper, index) => {
-    if (oper === "m") {
-      multipyPos.push(index);
-      operPos.multiply.push(index);
-    } if (oper === "d") {
-        dividePos.push(index);
-        operPos.divide.push(index);
-    } if (oper === "a") {
-        addPos.push(index);
-        operPos.add.push(index);
-    } if (oper === "s") {
-        subPos.push(index);
-        operPos.subtract.push(index);
-    }
-    return oper;
-});*/
 
 const totalNum = () => {
         //let sum = 0;
+        
+     
 
-//checkOper();
-   // console.log(operArray);
+   console.log(operArray);
     //console.log(operPos);
    /* if (operArray[1] === "=") {
         //numArray.splice(0, 2);
@@ -72,111 +58,94 @@ const totalNum = () => {
     //return sum;
 }
 
-const operate = () => {
+const multiplyDivide = () => {
     
-    for (let i = 0; i < 10; i++) {
-     operArray.map((oper, index) => {
-       // console.log("runs!!");
-    if (oper === "m") {
-        sum = numArray[index] * numArray[index + 1];
-        numArray.splice(index, 0, sum);
-        numArray.splice(index + 1, 2);
-        console.log(numArray);
+    for (let j = 0; j < 10; j++) {        
+    if (numArray.length !== 1) {
+     //   j = 0;
+    } 
+     if (arrStr.startsWith("s") || arrStr.startsWith("a") || arrStr.endsWith("s") || arrStr.endsWith("a")) {
+     j = operArray.findIndex(oper => oper === "m") || operArray.findIndex(oper => oper === "d");
+
+     } else if (operArray.indexOf("d") === 0 || operArray.indexOf("m") === 0) {
+         j = 0;
+         j++;
+ } else if (operArray.indexOf("d") === operArray.length -1 || operArray.indexOf("m") === operArray.length -1){
+    j = operArray.length - 1;
+    j--;
+ }
+ 
+ if (operArray[j] === "m") {
+     let index = operArray.findIndex(oper => oper === "m");
+     sum = numArray[index] * numArray[index + 1];
+     numArray.splice(index, 2, sum);
+     console.log(numArray);
+     operArray.splice(index, 1);    
+     console.log(operArray);
+ } else if (operArray[j] === "d") {
+     index = operArray.findIndex(oper => oper === "d");
+     sum = numArray[index] / numArray[index + 1];
+     numArray.splice(index, 2, sum);
+     console.log(sum);
+     console.log(numArray);
+     operArray.splice(index, 1);
+     console.log(operArray);
+ } 
+    else if (!operArray.includes("d") && !operArray.includes("m")) {
+     break;
+ }
+} console.log(numArray);    
+} 
+
+const operate = () => { 
+    multiplyDivide();
         
-                operArray.splice(index, 1);
+       if (operArray.includes("a") || operArray.includes("s")) {
+        addSubtract();
+   }
+}
+const addSubtract = () => {
+        for (let i = 0; i < 10; i++) {
             
-        console.log(operArray);
-    } if (oper === "d") {
-        sum = numArray[index] / numArray[index + 1];
-        numArray.splice(index, 0, sum);
-        console.log(sum);
-        numArray.splice(index + 1, 2);
-        console.log(numArray);
-        operArray.splice(index, 1);
-        console.log(operArray);
-    }
-        });
-
-}
-
-   /* let j = 0;
-    while (!operArray.includes("a")) {
-        operArray.map((oper, index) => {  
-    if (oper === "a") {
-        sum = numArray[index] + numArray[index + 1];
-        numArray.splice(index, 0, sum);
-        numArray.splice(index + 1, 2);
-        console.log(numArray);
-        operArray.map((oper, index) => {
-            if (oper === "a") {
-                oper.slice(index, 1);
-            }
-            return oper;
-        });
-        
-    }
-    }); 
-j++;
-}
-    let k = 0;
-   while (!operArray.includes("s")){
-    operArray.map((oper, index) => {  
-    if (oper === "s") {
-        sum = numArray[index] + numArray[index + 1];
-        numArray.splice(index, 0, sum);
-        numArray.splice(index + 1, 2);
-        console.log(numArray);
-    }
-    operArray.map((oper, index) => {
-        if (oper === "s") {
-            oper.slice(index, 1);
+            if (numArray.length !== 1) {
+                 // i = 0;
         }
-        return oper;
-    });
-   })
-k++;
-}*/
-return numArray;
-}
-
-
-        /*for (let i = 0; ; i++){
-            
-        if (operArray[i] === "m") {
-            sum = numArray[multipyPos.value] * numArray[multipyPos.value + 1];
-            numArray.splice(multipyPos.value, 0, sum);
-            numArray.splice(multipyPos.value + 1, 2);
-            operArray.map((oper, index) => {
-                if (oper === "m") {
-                    oper.slice(index, 1);
-                }
-                return oper;
-            });
-        } else if (operArray[i] === "d") {
-            sum = numArray[dividePos.value] / numArray[dividePos.value + 1];
-            numArray.splice(dividePos.value, 0, sum);
-            numArray.splice(dividePos.value + 1, 2);
-            operArray.map((oper, index) => {
-                if (oper === "d") {
-                    oper.slice(index, 1);
-                }
-                return oper;
-            });
-        } 
-        for ( let j = 0; ; j++) {
+        if ( arrStr.endsWith("d") || arrStr.endsWith("m") ||
+         arrStr.startsWith("d") || arrStr.startsWith("m")) {
+            multiplyDivide(operArray);
+            console.log(operArray);
+            break;
+        } else if (arrStr.startsWith("a") || arrStr.startsWith("s")) {
+            i =0; 
+            i++;
+        } else if (arrStr.endsWith("a") || arrStr.endsWith("s")) {
+            i = operArray.length -1;
+            i--;
+        }
         if (operArray[i] === "a") {
-            
-            sum = numArray[j] + numArray[i +1];
-            numArray.splice()
-            
-        } else if (dividePos[i] !== 0) {
-            sum = numArray[i] + numArray[i];
-        } else {
-            console.log("Error in totalNum()");
+          let  index2 = operArray.findIndex(oper => oper === "a");
+          console.log(numArray);
+            sum = numArray[index2] + numArray[index2 + 1];
+            console.log(sum);
+            numArray.splice(index2, 2, sum);
+            operArray.splice(index2, 1);  
+            console.log(operArray);
+            console.log(numArray);
+        } else if (operArray[i] === "s") {
+            index2 = operArray.findIndex(oper => oper === "s");
+            sum = numArray[index2] - numArray[index2 + 1];
+            numArray.splice(index2, 2, sum);
+            operArray.splice(index2, 1);  
+            console.log(operArray);
+            console.log(sum);
+            console.log(numArray);
+        }else if (!operArray.includes("a") && operArray.includes("s")) {
+            break;
         }
+    }//console.log(numArray);
     }
-    }
-}*/
+
+
 
 const addValues = e => {  
      total += e.target.value || e.key;
